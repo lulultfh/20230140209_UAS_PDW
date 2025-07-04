097a41fc3d003ed07d33f44529d901b737f23768
lululthfh <punyalulu05@gmail.com>
2025-07-04 20:46:04 +0700

    update db

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Pengaturan Database
-define('DB_SERVER', '127.0.0.1');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root'); 
 define('DB_PASSWORD', ''); 
-define('DB_NAME', 'pengumpulantugas');
+define('DB_NAME', 'database');
 
 // Membuat koneksi ke database
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
